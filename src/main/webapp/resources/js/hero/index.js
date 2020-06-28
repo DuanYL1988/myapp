2f1847e0dda8336e4c931a097b2901ba306904a0
duanYl <dylsweetdeath@gmail.com>
2020-06-28 14:39:29 +0900

    追加链接处理

--- src/main/webapp/resources/js/hero/index.js
+++ src/main/webapp/resources/js/hero/index.js
@@ -8,38 +8,51 @@ $(function() {
         console.log("display from static html");
         path = "../../../resources/images/feh/";
         // 静态页面加载json数据
-        ajaxGet("htmlJson.json",null,function(data){
+        ajaxGet("indexJson.json",null,function(data){
             var jsonData = data;
-            initlize(jsonData);
+            initlize(jsonData,htmlFlag);
         });
     } else {
         path = "/myapp/resources/images/feh/";
-        initlize(jsonData);
+        initlize(jsonData,htmlFlag);
     }
 });
 
 /**
  * 静态html打开时,读取json设置隐藏区域的图片
  */
-function initlize(jsonData){
+function initlize(jsonData,htmlFlag){
     $.each(jsonData,function(){
         /*
            只是单纯文字列,得到图片信息需要再次取得元素对象所以不使用
          var imgHtml = "<img src='#{src}'/>";
         */
+        // 图片div块
+        var imageDivEle = document.createElement('div');
+        imageDivEle.className = "imageArea";
+        // TODO 文字div
+        var txtDivEle = document.createElement('div');
+        txtDivEle.className = "textArea";
+        // 链接
+        var linkEle = document.createElement('a');
         // 直接创建img对象
         var imgEle = document.createElement('img');
         if(htmlFlag){
-            imgEle.src=path+this.imageSrc;
+            imgEle.src = path+this.htmlimagesrc;
+            linkEle.href = this.htmlurl;
         } else {
-            imgEle.src=path+this.htmlimagesrc;
+            imgEle.src = path+this.imageSrc;
+            linkEle.href = this.actionUrl;
         }
         // 加载图片信息需要一段时间
         sleep(100);
         // 取得最短列id
         var targetId = getShortDivId(imgEle.height);
+        linkEle.appendChild(imgEle);
+        imageDivEle.appendChild(linkEle);
         // 添加图片元素
-        document.getElementById(targetId).appendChild(imgEle);
+        document.getElementById(targetId).appendChild(imageDivEle);
+        document.getElementById(targetId).appendChild(txtDivEle);
     });
 }
 
