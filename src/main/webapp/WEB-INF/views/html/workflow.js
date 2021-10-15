78c5ab150cabec822e0fecc18bc4b6b69bd1377c
Duan Yl <63488574+DuanYL1988@users.noreply.github.com>
2021-10-15 22:21:48 +0900

    table追加对应

--- src/main/webapp/WEB-INF/views/html/workflow.js
+++ src/main/webapp/WEB-INF/views/html/workflow.js
@@ -1,32 +1,22 @@
 var CURRENT_STEP;
 var formEle;
 
+var status = 0;
 $(function() {
   // 初始设定
   CURRENT_STEP = wkJson.currentStep;
   formEle = document.getElementById("infoForm");
   initlize();
-  
   // 基本情报
   initEditArea(contract.baseInfo,formEle);
   // 默认表示区域
-  initEditArea(contract[wkJson.displayArea],formEle);
-  //
-  initlizeTable(masterInfo.tableInfo);
+  var displayA = wkJson.steps[CURRENT_STEP].clnm;
+  initEditArea(contract[displayA],formEle);
+  
 });
-
-function showSection(divEle){
-  var parentEle = $(divEle).parents()[0];
-  $(parentEle).children().attr('class','layout-title');
-  divEle.className = 'layout-title current-layout';
-  var nm = divEle.id.replace("title-","");
-  $("div[class^='group-section']").hide();
-  $("#"+nm).show();
-}
 
 /** 初期化处理 */
 function initlize(){
-  // 初始化
   document.getElementById("wk_icon_area").innerHTML = "";
   // 进度条
   $.each(wkJson.stepsStr,function(i,ele){
@@ -63,15 +53,19 @@ function initlize(){
     
   });
   
-  // 按钮区域初始化
+  // 按钮
   document.getElementById("eventArea").innerHTML = "";
-
   for(var i = wkJson.steps[CURRENT_STEP].actions.length; i > 0 ; i--) {
     var eventInfo = wkJson.steps[CURRENT_STEP].actions[i-1];
     var btnEle = createElement("button","","btn");
     btnEle.innerHTML = eventInfo.name;
-    btnEle.setAttribute("onclick","doEvent('"+eventInfo.step+"')");
-    
+    // 当前Step内状态切换
+    if (typeof eventInfo.step == "number") {
+      btnEle.setAttribute("onclick","doStep('"+eventInfo.step+"')");
+    } else {
+    // 前往下一个step
+      btnEle.setAttribute("onclick","doEvent('"+eventInfo.step+"')");
+    }
     // 添加
     document.getElementById("eventArea").appendChild(btnEle);
   }
@@ -84,10 +78,6 @@ function initlize(){
  * @param formEle 生产DIV的父DIV对象（Element）
  */
 function initEditArea(obj,parentEle){
-  // 
-  if (isEmpty(obj)) {
-    return;
-  }
   if (isNotEmpty(document.getElementById(obj.id))) {
     document.getElementById(obj.id).style.display = "";
     return;
@@ -136,7 +126,7 @@ function initEditArea(obj,parentEle){
       showSection(this);
     });
     
-  } else {
+  } else if ("single"==obj.layout){
     // 实际编辑区域
     var baseInfoEle = createElement("div",obj.id,"editArea");
     // 标题
@@ -144,140 +134,148 @@ function initEditArea(obj,parentEle){
     titleEle.innerHTML = obj.title;
     baseInfoEle.appendChild(titleEle);
     
-    $.each(obj.detail.rows,function(i,rows){
-      var inputRowEle = createElement("div","","inputRow");
-      // 排列方向
-      if ("vertical" == rows.design) {
-        inputRowEle.className = "inputRow vertical";
-      }
-      //prop
-      $.each(rows.items,function(j,prop){
-        var inputCell = createElement("div","","inputCell");
-        // 属性Title
-        var inputType = prop.type;
-        var lblEle = createElement("label","","");
-        if ("button" != inputType) {
+    // table
+    if (isNotEmpty(obj.head)){
+      console.log(obj.head);
+      // Table
+      var tableEle = createElement("table","","");
+      tableEle.cellspacing = "0";
+      tableEle.border = "1";
+      
+      // Thead
+      var thead = createElement("thead","","");
+      var tr = createElement("tr","","");
+      $.each(obj.head,function(){
+        var td = createElement("td","","");
+        td.innerHTML = this;
+        tr.appendChild(td);
+      });
+      thead.appendChild(tr);
+      tableEle.appendChild(thead);
+      
+      // Tbody
+      var tbody = createElement("tbody","","");
+      $.each(masterInfo[obj.info],function(){
+        tr = createElement("tr","","");
+        $.each(this,function(j,cell){
+          var td = createElement("td","","");
+          td.innerHTML = cell;
+          tr.appendChild(td);
+        });
+        tbody.appendChild(tr);
+      
+      });
+      tableEle.appendChild(tbody);
+      
+      baseInfoEle.appendChild(tableEle);
+      
+    } else {
+      
+      $.each(obj.detail.rows,function(i,rows){
+        var inputRowEle = createElement("div","","inputRow");
+        // 排列方向
+        if ("vertical" == rows.design) {
+          inputRowEle.className = "inputRow vertical";
+        }
+        //prop
+        $.each(rows.items,function(j,prop){
+          var inputCell = createElement("div","","inputCell");
+          // 属性Title
+          var lblEle = createElement("label","","");
           lblEle.innerHTML = prop.name;
           inputCell.appendChild(lblEle);
-        }
-        // 属性Input
-        var inputEle = createElement("input","","");
-        inputEle.name = obj.id + "." + prop.property;
-        inputEle.id = obj.id + "_" + prop.property;
-        
-        // 输入框
-        if ("text" == inputType) {
-          inputEle.type = "text";
-          inputEle.value = prop.value;
-          inputEle.style = prop.style;
-        // 下拉菜单
-        } else if ("select" == prop.type){
-          inputEle = createElement("select","","");
-          createOptions(inputEle,masterInfo[prop.masterId]);
+          // 属性Input
+          var inputEle = createElement("input","","");
+          var inputType = prop.type;
+          inputEle.style = isEmpty(prop.style) ? "":prop.style;
           
-        // Autocomplete
-        } else if ("autocomplete" == inputType) {
-          inputEle.type = "text";
-          inputEle.autocomplete = "on";
-          inputEle.setAttribute("list",prop.masterId);
-          inputEle.value = prop.value;
-          
-          var datalistEle = createElement("datalist",prop.masterId,"");
-          createOptions(datalistEle,masterInfo[prop.masterId]);
-          document.getElementById("hideArea").appendChild(datalistEle);
-          
-        // 单选,复选按钮
-        } else if ("radio" == inputType || "checkbox" == inputType){
-          inputEle = null;
-          // 属性设定
-          var eleInfo = {
-            "name" : obj.id + "." + prop.property,
-            "id" : "",
-            "classNm" : "",
-            "type" : inputType,
-            "require" : prop.require
-          };
-          createCheck(eleInfo,masterInfo[prop.masterId],inputCell);
-          
-        // 单纯表示项目
-        } else if ("label" == inputType){
-          inputEle = createElement("label","","");
-          inputEle.innerHTML = prop.value;
-          inputEle.style.width = "auto";
-        } else if ("textarea" == inputType){
-          inputEle = createElement("textarea","","");
-          inputEle.value = prop.value;
-          inputEle.rows = 7;
-          inputEle.cols = 80;
-        // 日期控件
-        } else if ("datetime" == inputType) {
-          inputEle.type = "datetime-local";
-          inputEle.className = "dateTime";
-          inputEle.value = prop.value;
-        // 按钮
-        } else if ("button" == inputType) {
-          inputEle = createElement("button","","");
-          inputEle.setAttribute("onclick",prop.value);
-          inputEle.innerHTML = prop.name;
-          inputEle.type = "button"
-          inputEle.className = "cell_btn";
-          inputEle.name = "";
-        }
-        
-        // 单选复选是对象为空
-        if (isNotEmpty(inputEle)) {
-          // require
-          if (isNotEmpty(prop.require)) {
-            inputEle.setAttribute("notempty","true");
+          // 输入框
+          if ("text" == inputType) {
+            inputEle.type = "text";
+            inputEle.value = prop.value;
+          // 下拉菜单
+          } else if ("select" == prop.type){
+            inputEle = createElement("select","","");
+            createOptions(inputEle,masterInfo[prop.masterId]);
+            
+          // Autocomplete
+          } else if ("autocomplete" == inputType) {
+            inputEle.type = "text";
+            inputEle.autocomplete = "on";
+            inputEle.setAttribute("list",prop.masterId);
+            inputEle.value = prop.value;
+            
+            var datalistEle = createElement("datalist",prop.masterId,"");
+            createOptions(datalistEle,masterInfo[prop.masterId]);
+            document.getElementById("hideArea").appendChild(datalistEle);
+            
+          // 单选,复选按钮
+          } else if ("radio" == inputType || "checkbox" == inputType){
+            inputEle = null;
+            // 属性设定
+            var eleInfo = {
+              "name" : obj.id + "." + prop.property,
+              "id" : "",
+              "classNm" : "",
+              "type" : inputType,
+              "require" : prop.require
+            };
+            createCheck(eleInfo,masterInfo[prop.masterId],inputCell);
+            
+          // 单纯表示项目
+          } else if ("label" == inputType){
+            inputEle = createElement("label","","");
+            inputEle.innerHTML = prop.value;
+            inputEle.style.width = "auto";
+          } else if ("textarea" == inputType){
+            inputEle = createElement("textarea","","");
+            inputEle.value = prop.value;
+            inputEle.rows = 7;
+            inputEle.cols = 80;
+          // 日期控件
+          } else if ("datetime" == inputType) {
+            inputEle.type = "datetime-local";
+            inputEle.className = "dateTime";
+            inputEle.value = prop.value;
+          // 按钮
+          } else if ("button" == inputType) {
+            inputEle = createElement("button","","cell_btn","");
+            inputEle.type = "button"
+            inputEle.setAttribute("onclick",prop.value);
+            inputEle.innerHTML = prop.name;
           }
-          // 只读
-          var displayEle = createElement("span","","displayOnly");
-          inputCell.appendChild(inputEle);
-          inputCell.appendChild(displayEle);
-
-        }
-        inputRowEle.appendChild(inputCell);
+          
+          if (isNotEmpty(inputEle)) {
+            // require
+            if (isNotEmpty(prop.require)) {
+              inputEle.setAttribute("notempty","true");
+            }
+            // 只读
+            var displayEle = createElement("span","","displayOnly");
+            inputCell.appendChild(displayEle);
+            inputEle.name = obj.id + "." + prop.property;
+            inputCell.appendChild(inputEle);
+          }
+          inputRowEle.appendChild(inputCell);
+        });
+        baseInfoEle.appendChild(inputRowEle);
       });
-      baseInfoEle.appendChild(inputRowEle);
-    });
-    
+    }
     parentEle.appendChild(baseInfoEle);
   }
 }
 
-function initlizeTable(arr){
-  if(isNotEmpty(arr)) {
-    $.each(arr,function(){
-      addTableRecord("display_table_body",this);
-    });
-  }
-}
 
 /**
- * 工作流动作
+ * LayoutGroup切换
  */
-function doEvent(step){
-  if (doValidation()) {
-    return;
-  }
-
-  var parentEle = arrIconReset();
-  // 对象取得
-  var targetEle = document.getElementById(step);
-  // 设置当前step
-  CURRENT_STEP = step
-
-  // 状态Step图标重设
-  arrIconSet(targetEle);
-  
-  // 内容刷新
-  $.each($(formEle).children(),function(){
-    if ("baseInfo" != this.id) {
-      $(this).hide();
-    }
-  });
-  var classNm = wkJson.steps[step].clnm;
-  initEditArea(contract[classNm],formEle);
+function showSection(divEle){
+  var parentEle = $(divEle).parents()[0];
+  $(parentEle).children().attr('class','layout-title');
+  divEle.className = 'layout-title current-layout';
+  var nm = divEle.id.replace("title-","");
+  $("div[class^='group-section']").hide();
+  $("#"+nm).show();
 }
 
 /**
@@ -325,74 +323,33 @@ function arrIconSet(targetEle){
   initlize();
 }
 
-function addRecord() {
-  var strTime = formateTypeDT("heathInfo_startTime");
-  var endTime = formateTypeDT("heathInfo_currentTime");
-  var info = $("#heathInfo_information").val();
-  var countermeasure = $("#heathInfo_countermeasure").val();
-  var timeDiff = getTimeDiff(strTime,endTime);
+function doStep(step) {
   
-  var columns = [$("#heathInfo_currentTime").val(),timeDiff.strHM,info+"<br>"+countermeasure];
-  addTableRecord("display_table_body",columns);
-  
-  getTableInfo();
-}
-
-function getTableInfo(){
-  var trEles = $("#display_table_body").find("tr");
-  var tableInfo = [];
-  $.each(trEles,function(){
-    var row = [];
-    $.each($(this).find("td"),function(i,tdEle){
-      row.push(tdEle.innerHTML);
-    });
-    tableInfo.push(row);
-  })
-  console.log(tableInfo);
 }
 
 /**
- * 对table添加行
+ * 工作流动作
  */
-function addTableRecord(tblBodyId,columns){
-  var trEle = createElement("tr","","");
-  $.each(columns,function(){
-    var tdEle = createElement("td","","");
-    tdEle.innerHTML = this;
-    trEle.appendChild(tdEle);
+function doEvent(step){
+  if (doValidation()) {
+    return;
+  }
+
+  var parentEle = arrIconReset();
+  // 对象取得
+  var targetEle = document.getElementById(step);
+  // 设置当前step
+  CURRENT_STEP = step;
+
+  // 状态Step图标重设
+  arrIconSet(targetEle);
+  
+  // 内容刷新
+  $.each($(formEle).children(),function(){
+    if ("baseInfo" != this.id) {
+      $(this).hide();
+    }
   });
-  document.getElementById(tblBodyId).appendChild(trEle);
-}
-
-/**
- * 对<input type="datetime">的值进行转换
- *
- * @param eleId 输入框的ID
- */
-function formateTypeDT(eleId){
-  var stTime = document.getElementById(eleId).value;
-  stTime = stTime.replace("T"," ");
-  stTime += ":00";
-  stTime = new Date(stTime);
-  return stTime;
-}
-
-/**
- * 计算时间差
- */
-function getTimeDiff(strTime,endTime){
-  var bt = (endTime-strTime)/1000/60;
-  var bth = parseInt(bt/60);
-  var btm = bt%60;
-  
-  var result = {
-    "strHM" : bth+"小时"+btm+"分",
-    "mSecond" : bt
-  };
-  return result;
-}
-
-
-function writeFile(){
-  
+  var classNm = wkJson.steps[step].clnm;
+  initEditArea(contract[classNm],formEle);
 }
