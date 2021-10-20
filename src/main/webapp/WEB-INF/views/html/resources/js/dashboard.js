584ebc9095e379e6f2f6fe9b1e754032da425365
Duan Yl <63488574+DuanYL1988@users.noreply.github.com>
2021-10-20 16:06:10 +0900

    更新

--- src/main/webapp/WEB-INF/views/html/resources/js/dashboard.js
+++ src/main/webapp/WEB-INF/views/html/resources/js/dashboard.js
@@ -1,50 +1,56 @@
 $(function() {
-  ctxPie = document.getElementById("pieChart").getContext("2d");
-  // 生成随机颜色
-  $.each(configPie.data.datasets[0].data,function(i,num){
-    configPie.data.datasets[0].backgroundColor.push(getRandomColor());
-  });
-  pieChart = new Chart(ctxPie, configPie);
+  drawPie("serviceType",serviceType);
+  drawPie("charge",charge);
+  drawPie("workStatus",workStatus);
 });
+
+function drawPie(eleId,obj){
+  ctxPie = document.getElementById(eleId).getContext("2d");
+  var configs = {
+    "type" : "pie",
+    "data": {
+      "datasets": [
+        {
+          "data": obj.count,
+          "backgroundColor": [
+          ]
+        },
+        {
+          "data": [getSum(obj.count)],
+          "backgroundColor": [
+            "white"
+          ],
+          "labels": []
+        }
+      ],
+      "labels": obj.label
+    },
+    "options": {
+      "responsive": false,
+      "maintainAspectRatio": true
+    }
+  };
+
+  // 生成随机颜色
+  $.each(obj.count,function(){
+    configs.data.datasets[0].backgroundColor.push(getRandomColor());
+  });
+  pieChart = new Chart(ctxPie, configs);
+
+}
+
+function getSum(arrs){
+  var sum = 0;
+  $.each(arrs,function(){
+    sum = sum + parseInt(this);
+  });
+  return sum;
+}
 
 /**
  * 生成随机颜色
  */
 function getRandomColor(){
-  return "rgb(" + Math.ceil(Math.random()*255) +","+Math.ceil(Math.random()*255) + "," +Math.ceil(Math.random()*255) + ",1)";
-}
-
-function temp(){
-  var configPie = {
-    "widht" : "400",
-    "height" : "400",
-    "radius" : "120"
-  };
-  drawPieChart("group",configPie);
-}
-
-/**
- * 绘制饼状图
- */
-function drawPieChart(canvasId,configPie) {
-  // 取得画布
-  var ctxPie = document.getElementById("group").getContext("2d");
-  // 取得中心点
-  var startX = configPie.widht/2;
-  var starty = configPie.height/2;
-
-  /**
-   * @param startX 中心点X
-   * @param starty 中心点Y
-   * @param r 半径
-   * @param start 初始位置(0为[startX+r,starty])
-   * @param end 结束位置(2PI为一周,圆的周长公式为2PIr)
-   */
-  ctxPie.beginPath();
-  ctxPie.fillStyle="#FFFF00";
-  ctxPie.arc(startX,starty,configPie.radius,-Math.PI/2,Math.PI/2,false);
-  ctxPie.lineTo(startX,starty);
-  ctxPie.lineTo(startX,starty-configPie.radius);
-  ctxPie.stroke();
-  
+  var color = "rgb(" + Math.ceil(Math.random()*255) +","+Math.ceil(Math.random()*255) + "," +Math.ceil(Math.random()*255) + ",1)";
+  return color;
 }
